87d70b6799441899755dff8b5c6e02663b28806c
Toheeb <aladetoheeb5@gmail.com>
2022-10-04 23:56:35 +0100

    Add automation for the configuration

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,6 @@
+# Installs Nginx and configure a server
+
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/alx-intranet.hbtn.io\/projects\/266 permanent;" /etc/nginx/sites-available/default ; sudo service nginx start',
+}
